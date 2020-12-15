d0cde38061168804126b6fa1cf367d5b6d810dbe
kzok <yes.notyet@gmail.com>
2020-12-15 22:51:35 +0900

    HTTP サーバのポートを環境変数から取るように変更

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,23 @@ import fastify from "fastify";
 
 import { logger } from "./logger";
 
-const HTTP_SERVER_PORT = 10666;
-const HTTP_SERVER_BIND = "0.0.0.0";
+/**
+ * Configurations
+ */
+
+const env = process.env;
+const HTTP_SERVER_PORT = parseInt(env.PDF_GEN_HTTP_PORT || "") || 10666;
+
+/**
+ * Entry point
+ */
 
 const server = fastify({ logger });
 server.get("/", async (_, res) => {
   await new Promise((done) => setTimeout(done, 3 * 1000));
   res.send({ hello: "world" });
 });
-server.listen(HTTP_SERVER_PORT, HTTP_SERVER_BIND, (err) => {
+server.listen(HTTP_SERVER_PORT, "0.0.0.0", (err) => {
   if (err) {
     throw err;
   }
