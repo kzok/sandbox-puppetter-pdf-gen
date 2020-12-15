8163e52d37bddde7f7bfc7657b53c4515dc544c1
kzok <yes.notyet@gmail.com>
2020-12-15 21:54:03 +0900

    3 秒遅れでレスポンスを返す Hello world サーバを実装

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-export const greet = (name: string) => `Hello, ${name}!`;
+import fastify from "fastify";
+
+const HTTP_SERVER_PORT = 10666;
+const HTTP_SERVER_BIND = "0.0.0.0";
+
+const server = fastify({ logger: true });
+server.get("/", async (_, res) => {
+  await new Promise((done) => setTimeout(done, 3 * 1000));
+  res.send({ hello: "world" });
+});
+server.listen(HTTP_SERVER_PORT, HTTP_SERVER_BIND, (err, address) => {
+  if (err) throw err;
+  console.info(`server listening on ${address}`);
+});
